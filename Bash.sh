e5c6d1f99785c7f1a49d706b5152032f86cb2e89
Your Name <you@example.com>
2019-07-15 22:33:32 +0530

    Bash script updated

--- Bash.sh
+++ Bash.sh
@@ -1,30 +1,34 @@
-Connect-AzAccount
-
+﻿
 $RG="PulumiRG"
+
+
 $port1=3307
+
 
 $rulename1="DisasterRecovery1"
 $rulename2="DisasterRecovery2"
 
+
 $N1="K3" #Change the Name according to need
 $N2="K6" #Change the Name according to need
-
 
 
 $resource = Get-AzResource | Where {$_.ResourceGroupName –eq $RG -and $_.ResourceType -eq "Microsoft.Network/networkSecurityGroups"} 
 $nsg1 = Get-AzNetworkSecurityGroup -Name $N1 -ResourceGroupName $RG
 $nsg2 = Get-AzNetworkSecurityGroup -Name $N2 -ResourceGroupName $RG
 
+
 # Add the inbound security rule.
-$nsg2 | Add-AzNetworkSecurityRuleConfig -Name $rulename1 -Description "AllowCommFromD2toD1" -Access Allow `
+$nsg2 | Add-AzNetworkSecurityRuleConfig -Name $rulename1 -Description "AllowCommFromD1toD2" -Access Allow `
     -Protocol * -Direction Inbound -Priority 2000 -SourceAddressPrefix "10.0.2.0/24" -SourcePortRange $port1 `
     -DestinationAddressPrefix "15.0.2.0/24" -DestinationPortRange $port1
 
 
 # Add the outbound security rule.
-$nsg1 | Add-AzNetworkSecurityRuleConfig -Name $rulename2 -Description "AllowCommFromD2toD1" -Access Allow `
+$nsg1 | Add-AzNetworkSecurityRuleConfig -Name $rulename2 -Description "AllowCommFromD1toD2" -Access Allow `
     -Protocol * -Direction Outbound -Priority 2000 -SourceAddressPrefix "10.0.2.0/24" -SourcePortRange $port1 `
     -DestinationAddressPrefix "15.0.2.0/24" -DestinationPortRange $port1
+
 
 # Update the NSG.
 $nsg1 | Set-AzNetworkSecurityGroup
